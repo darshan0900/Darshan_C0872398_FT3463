f80a26d0dfad89f07379855a77a5f88a9456a64e
Darshan Jain <c0872398@mylambton.ca>
2022-11-21 22:55:27 -0500

    added multiple test expressions for question 2

--- src/ArithmeticOperation.java
+++ src/ArithmeticOperation.java
@@ -6,26 +6,29 @@ class ArithmeticOperation {
 
     public static void main(String[] args) {
         // constructing the expression 3 + 2 * 5
-//        ArithmeticExp expression = new Sum(
-//                new Number(2),
-//                new Product(
-//                        new Product(new Number(7), new Number(2)),
-//                        new Number(3)
-//                )
-//        );
-//        ArithmeticExp expression = new Sum(
-//                new Number(2),
-//                new Product(
-//                        new Modulo(new Number(7), new Number(2)),
-//                        new Number(3)
-//                )
-//        );
-        ArithmeticExp expression = new Sum(
+        ArithmeticExp expression1 = new Sum(
+                new Number(2),
+                new Product(
+                        new Product(new Number(7), new Number(2)),
+                        new Number(3)
+                )
+        );
+        System.out.println(expression1 + " = " + expression1.evaluate());
+
+        ArithmeticExp expression2 = new Sum(
+                new Number(2),
+                new Product(
+                        new Modulo(new Number(7), new Number(2)),
+                        new Number(3)
+                )
+        );
+        System.out.println(expression2 + " = " + expression2.evaluate());
+
+        ArithmeticExp expression3 = new Sum(
                 new Number(3),
                 new Product(new Number(2), new Number(5))
         );
-
-        System.out.println(expression + " = " + expression.evaluate());
+        System.out.println(expression3 + " = " + expression3.evaluate());
     }
 
 }
